0d9e76b70835e7bfb588cea910f9ed605a972d4b
Cam the Kirby <92703353+CamtheKirby@users.noreply.github.com>
2023-06-19 18:02:43 -0500

    Fixed For Update 37

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,9 +2,10 @@ namespace Minions;
 
 public static class ModHelperData
 {
+    public const string WorksOnVersion = "37";
     public const string Name = "The Minions";
     public const string Description = "The Minions have arrived in BTD6. Fight bloons with these banana throwing machines.";
-    public const string Version = "2.0.0";
+    public const string Version = "2.0.1";
     public const string RepoOwner = "Commander-Cat101";
     public const string RepoName = "MinionsMod";
     public const string Author = "Commander__Cat";
